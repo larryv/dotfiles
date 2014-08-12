130a4a9b5bfc594927e0e8e9f0ae94b4e3b1799d
Lawrence Velázquez <larryv@alum.mit.edu>
2014-08-12 00:52:36 -0400

    Remove Colemak configuration.
    These now live in a separate repository dedicated to Colemak-specific
    nonsense (https://github.com/larryv/colemakerel).

--- colemakerel/module.mk
+++
@@ -1,6 +0,0 @@
-colemakerel_DOTFILES := $(subst lesskey,.colemakerel/less,$(colemakerel_DOTFILES))
-
-$(prefix)/.colemakerel/less: lesskey
-	@mkdir -p -- "$(dir $@)"
-	@lesskey --output="$@" "$<"
-	@printf "Wrote $@\n" >&2
