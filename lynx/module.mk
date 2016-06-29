3eab44ba2820bd75b23aec1ab45709dfe2de2394
Lawrence Velázquez <vq@larryv.me>
2016-06-29 02:08:22 -0400

    lynx: Respect $XDG_DOWNLOAD_DIR for SAVE_SPACE
    See https://www.freedesktop.org/wiki/Software/xdg-user-dirs.

--- lynx/module.mk
+++ lynx/module.mk
@@ -1,5 +1,5 @@
 lynx := $(prefix)/.lynx
 macros += lynx
 
-LYNX_SAVE := $(or $(wildcard ~/Downloads),$(wildcard ~))
+LYNX_SAVE := $(or $(XDG_DOWNLOAD_DIR),$(wildcard ~/Downloads),$(wildcard ~))
 macros += LYNX_SAVE
