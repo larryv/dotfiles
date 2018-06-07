45ac4cd900eb3ccd7a101783df85f1cfe35fe614
Lawrence Velázquez <vq@larryv.me>
2018-06-06 22:16:25 -0400

    Assume MacPorts is installed to /opt/local
    While it's technically possible to install MacPorts to an arbitrary
    location, in practice I've never done this.

--- macports/module.mk
+++ macports/module.mk
@@ -1,5 +1,2 @@
-MACPORTS := /opt/local
-export quoted_MACPORTS := $(call shellquote,$(value MACPORTS))
-
 macports_clean_files := macports/_zsh/macports.zprofile \
                         macports/_zsh/macports.zshenv
