7f71cd111d145856dbd406ea30c9b4b1ba0794d7
Lawrence Velázquez <vq@larryv.me>
2020-02-11 03:19:48 -0500

    zsh: Rename update_terminal_pwd to update_terminal_cwd
    This matches the similar function in macOS's /etc/bashrc_Apple_Terminal.

--- zsh/module.mk
+++ zsh/module.mk
@@ -1,5 +1,5 @@
 zsh_files := zsh/_zsh/_zlogin \
              zsh/_zsh/_zprofile \
              zsh/_zsh/_zshrc \
-             zsh/_zsh/functions/update_terminal_pwd \
+             zsh/_zsh/functions/update_terminal_cwd \
              zsh/_zshenv
