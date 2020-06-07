9f1e0af192ae0a648d4ee0f68c1e6bb6b37f1bd0
Lawrence Velázquez <vq@larryv.me>
2020-06-07 16:16:44 -0400

    zsh: Move .zshenv into .zsh and make it visible
    I don't think .zshenv should be separated from its friends anymore. Move
    it to .zsh/zshenv and create the necessary symbolic link at ~/.zshenv.

--- zsh/module.mk
+++ zsh/module.mk
@@ -1,15 +1,15 @@
 zsh_files := zsh/_zsh/functions/update_terminal_cwd \
              zsh/_zsh/zlogin \
              zsh/_zsh/zprofile \
-             zsh/_zsh/zshrc \
-             zsh/_zshenv
+             zsh/_zsh/zshenv \
+             zsh/_zsh/zshrc
 
 # .zprofile relies on .profile to do most of its work.
 zsh-install: sh-install
 
 # Create links with the hidden names zsh requires.
-zsh_link_srcs := zlogin zprofile zshrc
-zsh_link_tgts := ~/.zsh/.zlogin ~/.zsh/.zprofile ~/.zsh/.zshrc
+zsh_link_srcs := zlogin zprofile .zsh/zshenv zshrc
+zsh_link_tgts := ~/.zsh/.zlogin ~/.zsh/.zprofile ~/.zshenv ~/.zsh/.zshrc
 zsh-install:
 	$(call gencmds,ln -fs,$(zsh_link_srcs),$(zsh_link_tgts))
 zsh-uninstall:
