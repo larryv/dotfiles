9f995cbe937844f18d55662024b243c2efe1db70
Lawrence Velázquez <vq@larryv.me>
2020-06-07 16:16:44 -0400

    zsh: Make hidden files in .zsh visible
    It doesn't make much sense for the zsh startup files to be hidden while
    being located in the .zsh directory, which is already hidden. Most other
    files in that directory are not hidden, including the topic startup
    files. (This thought did occur to me when I created .zsh, but for some
    reason all I did was rename the history file [*].)
    
    Rename the startup files without leading dots and create symbolic links
    at the old names so zsh can still find them. Use a non-dotted name for
    the completion system's dumped configuration, instead of the default
    ".zcompdump".
    
        [*]: commit d739a681189b04e83f7effc3e466968177f4b4e8

--- zsh/module.mk
+++ zsh/module.mk
@@ -1,8 +1,16 @@
-zsh_files := zsh/_zsh/_zlogin \
-             zsh/_zsh/_zprofile \
-             zsh/_zsh/_zshrc \
-             zsh/_zsh/functions/update_terminal_cwd \
+zsh_files := zsh/_zsh/functions/update_terminal_cwd \
+             zsh/_zsh/zlogin \
+             zsh/_zsh/zprofile \
+             zsh/_zsh/zshrc \
              zsh/_zshenv
 
 # .zprofile relies on .profile to do most of its work.
 zsh-install: sh-install
+
+# Create links with the hidden names zsh requires.
+zsh_link_srcs := zlogin zprofile zshrc
+zsh_link_tgts := ~/.zsh/.zlogin ~/.zsh/.zprofile ~/.zsh/.zshrc
+zsh-install:
+	$(call gencmds,ln -fs,$(zsh_link_srcs),$(zsh_link_tgts))
+zsh-uninstall:
+	$(RM) $(zsh_link_tgts)
