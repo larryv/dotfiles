27274b235c07f90fd16caa0b7d7ebd8f29c85152
Lawrence Velázquez <vq@larryv.me>
2020-07-07 17:48:48 -0400

    java: Don't override a preexisting JAVA_HOME
    I haven't actually encountered a need for this; it is purely preemptive.

--- java/_profile.d/java.sh
+++ java/_profile.d/java.sh
@@ -1,8 +1,10 @@
 # On Mac OS X / OS X / macOS, set JAVA_HOME if a JVM is enabled. I don't know
 # how to do this on other systems, or whether it's even necessary.
 
-# https://www.etalabs.net/sh_tricks.html, § "Getting non-clobbered output from
-# command substitution"
-JAVA_HOME=$(/usr/libexec/java_home --failfast 2>/dev/null && echo x) \
-    && export JAVA_HOME="${JAVA_HOME%??}" \
-    || unset JAVA_HOME
+if [ -z "$JAVA_HOME" ]; then
+    # https://www.etalabs.net/sh_tricks.html, § "Getting non-clobbered output
+    # from command substitution"
+    JAVA_HOME=$(/usr/libexec/java_home --failfast 2>/dev/null && echo x) \
+        && export JAVA_HOME="${JAVA_HOME%??}" \
+        || unset JAVA_HOME
+fi
