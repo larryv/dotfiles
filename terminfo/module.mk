dfc091c5251d290211c332fb1b084cfcfeabdb5a
Lawrence Velázquez <vq@larryv.me>
2023-02-27 18:58:26 -0500

    terminfo: Skip by default
    The terminfo.sh topic script has done nothing since 67c821f (terminfo:
    Stop setting TERM for Apple Terminal, 2021-07-12), but I don't remember
    why I opted to make it a big no-op instead of simply *not installing
    it*.  Maybe I just didn't think it through.  Wouldn't be the first time!

--- terminfo/module.mk
+++ terminfo/module.mk
@@ -15,16 +15,32 @@
 # <https://creativecommons.org/publicdomain/zero/1.0/>.
 
 
-all: terminfo
+# This module is temporarily being skipped by the "global" targets (all,
+# install, installdirs, and uninstall) because it doesn't work.
+#
+# When TERM is set to "nsterm-build400" and my update_terminal_cwd zsh
+# function is active, Apple Terminal appears to restore old values of
+# the current working directory/document when certain programs are run.
+# This can be seen by starting a new terminal, running and quitting
+# less(1), using cd(1) to switch to another directory, and running
+# less(1) again.  At this point Terminal's current working directory is
+# not the new directory but the one in which less(1) was run initially.
+# This also occurs with vim(1), nano(1), and no doubt other software,
+# but it does NOT occur when using less(1) via `git diff`.
+#
+# I think this has something to do with the alternate screen, but that's
+# just a hunch, and I have not yet attempted to look into it.
+
+#all: terminfo
 terminfo: terminfo/_profile.d/terminfo.sh FORCE
 
-installdirs: terminfo-installdirs
+#installdirs: terminfo-installdirs
 terminfo-installdirs: sh-installdirs FORCE
 
-install: terminfo-install
+#install: terminfo-install
 terminfo-install: sh-install terminfo terminfo-installdirs FORCE
 	$(INSTALL_DATA) terminfo/_profile.d/terminfo.sh ~/.profile.d/
 
-uninstall: terminfo-uninstall
+#uninstall: terminfo-uninstall
 terminfo-uninstall: FORCE
 	rm -f ~/.profile.d/terminfo.sh
